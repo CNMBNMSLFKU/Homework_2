62d01f0dd9346cba6809f5a2456c79478315f925
1pccx <121043242+1pccx@users.noreply.github.com>
2025-06-10 20:02:34 +0800

    Update main.cpp

--- homework/src/HeapSort/main.cpp
+++ homework/src/HeapSort/main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <stdexcept>
 
+int main() {
+    MinHeap<int> h;
+    h.Push(5);
+    h.Push(2);
+    h.Push(8);
+    h.Push(1);
+    h.Push(-10);
+    h.Push(2);
+    h.Push(INT_MAX);
+    h.Push(INT_MIN);
+
+
+    // 依序輸出最小元素，並移除
+    while (!h.IsEmpty()) {
+        std::cout << h.Top() << " ";
+        h.Pop();
+    }
+    return 0;
+}
